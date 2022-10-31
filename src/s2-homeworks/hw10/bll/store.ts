e49f1c1bcfcb6679a235efb7b41c35b14a239fe9
sanyalu <sanyalu97@gmail.com>
2022-10-31 21:48:03 +0100

    HW12

--- src/s2-homeworks/hw10/bll/store.ts
+++ src/s2-homeworks/hw10/bll/store.ts
@@ -13,5 +13,6 @@ export default store
 
 export type AppStoreType = ReturnType<typeof reducers>
 
+
 // @ts-ignore
 window.store = store // for dev // для того чтобы автотесты видели состояние данных
--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,14 +1,28 @@
-const initState = {
+
+
+export type ThemeStateType = { themeId: number }
+type ActionType = ChangeThemeIdActionType
+
+
+const initState: ThemeStateType = {
     themeId: 1,
 }
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+
+export const themeReducer = (state: ThemeStateType = initState, action: ActionType): ThemeStateType => { // fix any
     switch (action.type) {
         // дописать
-
+        case "SET_THEME_ID": {
+            return {
+                ...state,
+                themeId: action.id
+            }
+        }
         default:
             return state
     }
 }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+
+type ChangeThemeIdActionType = { type: 'SET_THEME_ID', id: number }
+export const changeThemeId = (id: number): ChangeThemeIdActionType => ({type: 'SET_THEME_ID', id}) // fix any
